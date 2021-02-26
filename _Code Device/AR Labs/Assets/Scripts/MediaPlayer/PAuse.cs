0c4546a728d6731eda2cc486adf150d51c1de0a2
Isaac Shirk <irs2e@mtmail.mtsu.edu>
2021-02-26 09:45:06 -0600

    Import Media Player files into AR Labs Project
    Put all Media Player code and scenes into its own folders,
    also put all media and art assets into a folder to reduce
    file tree clutter

---
+++ _Code Device/AR Labs/Assets/Scripts/MediaPlayer/PAuse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PAuse : MonoBehaviour
+{
+    public static int Flage = 1;
+    public void HandleOnClickEvent()
+    {
+        // Send a signal to pause or unpause the audio
+       
+        if (Flage == 0)
+        {
+            gameObject.SendMessage("Pause", -2);
+            Flage = 1;
+        }
+        else
+        {
+            gameObject.SendMessage("Pause", -1);
+            Flage = 0;
+        }
+    }
+}
---
+++ _Code Device/AR Labs/Assets/Scripts/MediaPlayer/Stop.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stop : MonoBehaviour
+{
+    public void HandleOnClickEvent()
+    {
+       //Send a signal to stop audio and video
+        gameObject.SendMessage("StopAudio", -1);
+       
+        gameObject.SendMessage("StopV", -1);
+    }
+}
