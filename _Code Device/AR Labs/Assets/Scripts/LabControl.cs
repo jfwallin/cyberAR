0d8a8a8bf71fc6ab21e8e0696483ffca5f308e01
Isaac Shirk <irs2e@mtmail.mtsu.edu>
2021-03-19 09:55:30 -0500

    Working on storing media names correctly, implementing lab control

--- _Code Device/AR Labs/Assets/Scripts/LabControl.cs
+++ _Code Device/AR Labs/Assets/Scripts/LabControl.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 public class LabControl : MonoBehaviour
 {
     private Camera mainCam = null;
+    [SerializeField]
+    private GameObject labStartButtonPrefab = null;
+    private Button labStartButton = null;
     [SerializeField]
     private GameObject mediaPlayerPrefab = null;
     private MediaPlayer mPlayer = null;
@@ -13,17 +18,33 @@ public class LabControl : MonoBehaviour
     private GameObject mcqPrefab = null;
     private MCQ.MCQManager mcqManager = null;
 
+    //private LabData labData = null;
+
     public void Awake()
     {
+        Assert.IsNotNull(labStartButton);
         Assert.IsNotNull(mediaPlayerPrefab);
         Assert.IsNotNull(mcqPrefab);
         mainCam = Camera.main;
     }
 
+    public void Start()
+    {
+        //Place the media player in front of the camera, hopefully this is a good position for now.
+        //also get a reference to the media player
+        mPlayer = Instantiate(mediaPlayerPrefab, mainCam.transform.position + Vector3.forward, Quaternion.identity).GetComponent<MediaPlayer>();
+
+        //Show beginning of lab splash screen
+        mPlayer.PlayMedia("_welcome", MCQ.MediaType.Image, IntroMediaComplete);
+
+        //Add start button
+        Button labStartButton = Instantiate(labStartButtonPrefab, mPlayer.transform.position + Vector3.down * 0.5f, Quaternion.identity).GetComponent<Button>();
+        labStartButton.onClick.AddListener(StartLab);
+    }
+
     public void StartLab()
     {
-        mPlayer = Instantiate(mediaPlayerPrefab, mainCam.transform.position + Vector3.forward, Quaternion.identity).GetComponent<MediaPlayer>();
-        mPlayer.PlayMedia("introVid", MCQ.MediaType.Video, IntroMediaComplete);
+        labStartButton.onClick.RemoveAllListeners();
     }
 
     public void IntroMediaComplete()
--- _Code Device/AR Labs/Assets/Scripts/MediaPlayer/ScriptSuggestions/MediaPlayer.cs
+++ _Code Device/AR Labs/Assets/Scripts/MediaPlayer/ScriptSuggestions/MediaPlayer.cs
@@ -20,23 +20,32 @@ public class MediaPlayer : MonoBehaviour
     public void PlayMedia(string mediaName, MCQ.MediaType mediaType, System.Action callback)
     {
         mediaCompleteCallback = callback; //Hold on to the function to call once the media is done
-        Debug.Log($"Play Media called with media name: {mediaName}\nExecuting callback\n\n\n");
+        Debug.Log($"Play Media called with media name: {mediaName}\n" +
+                  $"MediaType is {mediaType}\n");
 
         //First, play the media. If its a video or audio, wait until it is complete and then call the callback
         switch(mediaType)
         {
             case MCQ.MediaType.Audio:
+                AudioClip audio = mediaManager.GetAudioClip(mediaName);
+                Debug.Log($"MediaManager returned {audio?.name}");
                 break;
             case MCQ.MediaType.AudioAndImage:
+                AudioClip audioI = mediaManager.GetAudioClip(mediaName.Split(',')[0].Trim());
+                Texture2D imageA = mediaManager.GetImage(mediaName.Split(',')[1].Trim());
+                Debug.Log($"MediaManager returned {audioI?.name} and {imageA?.name}");
                 break;
             case MCQ.MediaType.Image:
+                Texture2D image = mediaManager.GetImage(mediaName);
+                Debug.Log($"MediaManager returned {image?.name}");
                 break;
             case MCQ.MediaType.Video:
-                //Get clip from resources
-                VideoClip video = (VideoClip)Resources.Load("Video/" + mediaName);
-                //or get the clip from the mediaManager
-                VideoClip video1 = mediaManager.GetVideoClip(mediaName);
+                //get clip from resources
+                //videoclip video = (videoclip)resources.load("video/" + medianame);
 
+                //or get the clip from the mediaManager
+                VideoClip video = mediaManager.GetVideoClip(mediaName);
+                Debug.Log($"MediaManager returned {video?.name}");
 
                 //Assign the new video clip
                 vPlayer.clip = video;
@@ -48,6 +57,7 @@ public class MediaPlayer : MonoBehaviour
                 vPlayer.Play();
                 break;
             case MCQ.MediaType.None:
+                Debug.Log("MediaType none encountered, no media retrieved");
                 break;
         }
     }
