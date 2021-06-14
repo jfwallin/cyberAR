b7ecb95c6fdf6068c295b25221f4947c58196abb
Isaac Shirk <irs2e@mtmail.mtsu.edu>
2021-06-14 09:41:15 -0500

    Activity Base Classes
    Added base classes for activity modules and data
    allowing them to be interacted with more generally.
    Also removed some dependencies on old moon lab
    resources.

---
+++ _Code Device/AR Labs/Assets/Scripts/Activity Modules/ActivityModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class defining the common functionality of an Activity Module
+/// </summary>
+public abstract class ActivityModule : MonoBehaviour
+{
+    public ActivityModuleData data;
+
+    public abstract void Initialize(ActivityModuleData initData);
+    //Have to define one function with one parameter. Problem is,
+    //different modules will have different data objects. If we 
+    //pass the unparsed string, that could work. and then the module
+    // could parse it itself and construct the data object it needs.
+    //this would require some restructuring of existing code.
+
+    public abstract void EndOfModule();
+
+    public abstract ActivityModuleData SaveState();
+}
---
+++ _Code Device/AR Labs/Assets/Scripts/Activity Modules/ActivityModuleData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for the serializeable data classes describing 
+/// activity modules
+/// </summary>
+[System.Serializable]
+public class ActivityModuleData
+{
+    public string moduleName;
+    public string specificName;
+    public string[] prerequisiteActivities;
+    public string[] educationalObjectives;
+    public string[] instructions;
+    public string[] introMediaIDs;
+    public string[] outroMediaIDs;
+    public int numRepeatsAllowed;
+    public int numGradableRepeatsAllowed;
+    public string gradingCriteria;
+    public float currentScore;
+    public float bestScore;
+    public bool completed;
+    public int currentSubphase;
+    public string[] subphaseNames;
+}
