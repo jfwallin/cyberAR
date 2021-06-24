8d89a8019482fd87dd31143109e6349a44366fbe
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2021-06-24 04:47:06 -0500

    cleaner examples
    kinda

---
+++ _Code Device/AR Labs/Assets/JSON Bridge/Examples/Solar System Example/MakeModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MakeModule : MonoBehaviour
+{
+    private Bridge bridge = new Bridge();
+
+    public string path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StreamReader reader = new StreamReader(path);
+        string line;
+
+        line = reader.ReadToEnd();
+
+        Debug.Log(line);
+        bridge.ParseJson(line);
+    }
+}
