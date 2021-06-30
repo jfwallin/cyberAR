e62e9a1916cd0bd54eed95e65463bdb5851c5211
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2021-06-30 01:08:31 -0500

    simple json maker update
    can now be used to edit jsons

--- _Code Device/AR Labs/Assets/JSON Bridge/Making JSONs temp/MakeJSON.cs
+++ _Code Device/AR Labs/Assets/JSON Bridge/Making JSONs temp/MakeJSON.cs
@@ -12,15 +12,35 @@ public class MakeJSON : MonoBehaviour
 {
 
     public string name;
-    public string folderPath;
+    public string path;
+    public bool newJson;
 
     public ObjectInfoCollection info;
+
+    private string json;
 
     // Start is called before the first frame update
     void Start()
     {
-        string json;
-        string path = folderPath + name + ".json";
+        
+        if (newJson)
+        {
+            path = path + name + ".json";
+        }
+        else
+        {
+            StreamReader reader = new StreamReader(path);
+            json = reader.ReadToEnd(); //read into from doc into string
+
+            info = JsonUtility.FromJson<ObjectInfoCollection>(json);
+        }
+        
+    }
+
+
+    void OnDestroy()
+    {
+        Debug.Log("in destroy");
 
         StreamWriter writer = new StreamWriter(path);
 
@@ -31,5 +51,4 @@ public class MakeJSON : MonoBehaviour
 
         writer.Close();
     }
-
 }
