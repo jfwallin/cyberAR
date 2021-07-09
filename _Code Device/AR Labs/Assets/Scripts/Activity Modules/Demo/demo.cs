a24476ff08622af2cc42aa7f76fdc9eda715c860
John Wallin <jfwallin@gmail.com>
2021-07-08 19:16:22 -0500

    added a singleton class for lighting control and an audio source in the demo module

--- _Code Device/AR Labs/Assets/Scripts/Activity Modules/Demo/demo.cs
+++ _Code Device/AR Labs/Assets/Scripts/Activity Modules/Demo/demo.cs
@@ -9,44 +9,46 @@ namespace demoRoutines
 {
     public class demo : ActivityModule
     {
-        private MediaPlayer mPlayer = null;         //Reference to the object that plays all media
         private demoData moduleData;        //Conatians answer choices, correct answers, and question text
-        private Bridge bridge;
         private string jsonString;
 
-        private lightingControl lightControl; 
+        private MediaPlayer mPlayer = null;         //Reference to the object that plays all media
+        private lightingControl lightControl;
+        private AudioSource aud;
+        private Bridge bridge;
 
 
         //public override void Initialize(ActivityModuleData dataIn)
         public override void Initialize(string jsonData)
         {
+            // save the json string into a private variable
             moduleData = new demoData();
-
             jsonString = jsonData;
             JsonUtility.FromJsonOverwrite(jsonData, moduleData);
+            
             mPlayer = MediaPlayer.Instance;
+            lightControl = lightingControl.Instance;
+            aud = gameObject.GetComponent<AudioSource>();
+            
+            aud.clip = Resources.Load<AudioClip>(moduleData.introAudio);
+            aud.Play();
 
-            /*
-            GameObject lc = GameObject.Find("lightingControl");
-            lightControl = lc.GetComponent<lightingControl>().Instance;
-
-          //  lightControl = new lightControl();
             if (moduleData.useSunlight)
                 lightControl.sunlight();
-            */
 
             bridge = new Bridge();
             if (moduleData.createObjects)
                 bridge.ParseJson(jsonString);
+
             if (moduleData.timeToEnd > 0)
                 StartCoroutine(EndByTime());
         }
 
         public override void EndOfModule()
         {
-            /*if (moduleData.restoreLights)
+            if (moduleData.restoreLights)
                 lightControl.restoreLights();   
-            */
+            
             if (moduleData.destroyObjects)
                 bridge.CleanUp(jsonString); 
 //            StartCoroutine(DelayBeforeExit());
