78f1cfed9a4b7fb96e3ec858093ff85199ae70db
Nicok01 <85763886+Nico-Kasz@users.noreply.github.com>
2021-07-21 15:01:11 -0500

    Adding Login Scene
    + Almost completed Login Scene added
    + VR Keyboard assets added

---
+++ _Code Device/AR Labs/Assets/Login Scene/autofill.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class autofill : MonoBehaviour
+{
+    #region Public Variables 
+    [Tooltip("This is how many characters are typed until a new list of names is requested.")]
+    public int lengthQueue = 3;
+    public int optionLimit = 5;
+    #endregion 
+
+    #region Private Variables
+    private List<string> names = new List<string>();
+    private Text input;
+    private string currText;
+    private Dropdown dropdown; 
+    private int showing = 0;
+    private List<User> users = new List<User>();
+    public Dictionary<string, Dictionary<string, string>> crnLabsUrl;
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialize textfield 
+        input = transform.GetChild(0).GetComponent<Text>();
+        dropdown = GetComponent<Dropdown>();
+        currText = input.text;
+        refreshText();
+
+
+        // TESTING - Name bank to pull autofill from 
+        // string[] nameslist = new string[] { "guest", "jim", "sam", "chad", "cronie", "crestin", "crestin2", "crestion", "crestolemu", "crester", "crestunker", "Michael", "Christopher", "Jessica", "Matthew", "Ashley", "Jennifer", "Joshua", "Amanda", "Daniel", "David", "James", "Robert", "John", "Joseph", "Andrew", "Ryan", "Brandon", "Jason", "Justin", "Sarah", "William", "Jonathan", "Stephanie", "Brian", "Nicole", "Nicholas", "Anthony", "Heather", "Eric", "Elizabeth", "Adam", "Megan", "Melissa", "Kevin", "Steven", "Thomas", "Timothy", "Christina", "Kyle", "Rachel", "Laura", "Lauren", "Amber", "Brittany", "Danielle", "Richard", "Kimberly", "Jeffrey", "Amy", "Crystal", "Michelle", "Tiffany", "Jeremy", "Benjamin", "Mark", "Emily", "Aaron", "Charles", "Rebecca", "Jacob", "Stephen", "Patrick", "Sean", "Erin", "Zachary", "Jamie", "Kelly", "Samantha", "Nathan", "Sara", "Dustin", "Paul", "Angela", "Tyler", "Scott", "Katherine", "Andrea", "Gregory", "Erica", "Mary", "Travis", "Lisa", "Kenneth", "Bryan", "Lindsey", "Kristen", "Jose", "Alexander", "Jesse", "Katie", "Lindsay", "Shannon", "Vanessa", "Courtney", "Christine", "Alicia", "Cody", "Allison", "Bradley", "Samuel", "Shawn", "April", "Derek", "Kathryn", "Kristin", "Chad", "Jenna", "Tara", "Maria", "Krystal", "Jared", "Anna", "Edward", "Julie", "Peter", "Holly", "Marcus", "Kristina", "Natalie", "Jordan", "Victoria", "Jacqueline", "Corey", "Keith", "Monica", "Juan", "Donald", "Cassandra", "Meghan", "Joel", "Shane", "Phillip", "Patricia", "Brett", "Ronald", "Catherine", "George", "Antonio", "Cynthia", "Stacy", "Kathleen", "Raymond", "Carlos", "Brandi", "Douglas", "Nathaniel", "Ian", "Craig", "Brandy", "Alex", "Valerie", "Veronica", "Cory", "Whitney", "Gary", "Derrick", "Philip", "Luis", "Diana", "Chelsea", "Leslie", "Caitlin", "Leah", "Natasha", "Erika", "Casey", "Latoya", "Erik", "Dana", "Victor", "Brent", "Dominique", "Frank", "Brittney", "Evan", "Gabriel", "Julia", "Candice", "Karen", "Melanie", "Adrian", "Stacey", "Margaret", "Sheena", "Wesley", "Vincent", "Alexandra", "Katrina", "Bethany", "Nichole", "Larry", "Jeffery", "Curtis", "Carrie", "Todd", "Blake", "Christian", "Randy", "Dennis", "Alison", "Trevor", "Seth", "Kara", "Joanna", "Rachael", "Luke", "Felicia", "Brooke", "Austin", "Candace", "Jasmine", "Jesus", "Alan", "Susan", "Sandra", "Tracy", "Kayla", "Nancy", "Tina", "Krystle", "Russell", "Jeremiah", "Carl"}; // TESTING
+        // foreach (string name in nameslist) { names.Add(name.ToLower()); }    
+        names.Add("guest"); users.Add(new User("guest","guest","0000000")); crnLabsUrl = new Dictionary<string, Dictionary<string, string>>() {{"0000000",new Dictionary<string, string>() {{"guest_Lab","www.guestLab.json/url"}}}};
+
+        // building localized User databases
+        crnLabsUrl = pullCSV(); // NOT WORKING ON LEAP
+        sort(names);
+
+        // Print out User and crn data to make sure everything is loading in properly 
+        // foreach (string crn in crnLabs.Keys) { string outtie = ""; foreach (string str in crnLabs[crn]) { outtie += str + " "; } print(outtie);  }
+        
+        // Prints each user with crn and password for testing
+        // foreach (User usr in users) { print(usr.ToString()); }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Only way to consistantly update and make it visible 
+        if (input.text.Length >= lengthQueue && showing > 0) { dropdown.Show(); }
+        else { dropdown.Hide(); }
+
+        // On change of text 
+        if (!currText.Equals(input.text))
+        {
+            currText = input.text;
+            print("Username updated to: " + currText);     // for debugging
+
+            // update options 
+            updateDropdown();
+        }
+    }
+
+
+    #region Public Methods 
+    // helps re-adjust dropdown options if authentication fails
+    public void refreshText()
+    {
+        currText = input.text;
+        showing = 0;
+
+    }
+
+
+    // Authenticates given usr/pas - NOT TO BE STORED ON DEVICE LONGTERM
+    public bool authenticate(string usr, string pas)
+    {
+        try {
+            // Find User
+            User user = users.Find(x => x.usr.Equals(usr));
+
+            // Check if pass is correct
+            bool authenticated = user.pas.Equals(pas);
+            Debug.Log("Username: " + usr + ", Password: " + pas + "\n\t    Authenticated: " + authenticated);
+            return authenticated;
+        } catch  {
+            print("Authentication Failed: Invalid Username");
+            return false;
+        }
+    }
+
+
+    // Called when textbox is changed: checks if enough letters are typed before giving up to a set limit of possible usernames
+    public void updateDropdown()
+    {
+        // Helps refresh dropdown menu
+        dropdown.Hide(); 
+
+
+        if (input.text.Length >= lengthQueue)
+        {
+            // Empty list before adding elemnts
+            dropdown.options.Clear();   
+            int count = 0;
+
+            // Cycle names list and pick ones that contain provided string
+            foreach (string name in names)
+            {
+                if (name.Contains(input.text) && count < optionLimit)
+                {
+                    dropdown.options.Add(new Dropdown.OptionData(name));
+                    count++;
+                }
+            } 
+            showing = count;    // Ensures that if no options appear, a blank dropdown won't appear
+        }
+
+    }
+
+
+    // Returns a Dictionary of <string lab, string jsonURL>
+    // Can only be called after the user is authenticated -> throws error if cant find CRN provided for authenticated user in Dictionary
+    public Dictionary<string,string> getLabs(string usr)
+    {
+        string crn = "0000000";  // Guest crn: seven 0s
+        try { 
+            crn = users.Find(x => x.usr.Equals(usr)).crn; 
+        } catch 
+        { 
+            Debug.Log("CSV not loaded, or forced next();");
+        }
+
+        try { 
+            return crnLabsUrl[crn]; 
+        } catch 
+        { 
+            print("CRN Dictionary is missing, returning a blank dicitonary"); 
+            return new Dictionary<string, string>(){ { "guest","default_json_url"} }; 
+        }
+    }
+    #endregion
+
+
+    #region Private Methods 
+    /* Loads names into a list, Username+Password+CRN into a list of UserObjects, and CRN + Hashset<labs> in a dictionary.
+     * @return: Dictionary of crns with set of lab IDs.
+     */
+    private Dictionary<string, Dictionary<string, string>> pullCSV()
+    {
+        var result = new Dictionary<string, Dictionary<string, string>>();
+
+        // Set path for where to pull data from
+        string namesPath = "Assets/Login Scene/csv bank/test_names.csv";
+        string crnPath = "Assets/Login Scene/csv bank/crn_to_labs.csv";
+
+        // Get String array of the lines and read them off
+        string[] lines = System.IO.File.ReadAllLines(namesPath);      
+
+        // 0: username, 1: Name, 2: CRN, 3: Instructor, 4: Password
+        for (int i = 1; i < lines.Length; i++)  // Skips labeling row
+        {
+            // Split into columns
+            string[] columns = lines[i].Split(',');
+
+            // Add names into autofill list and create new User(username, passowrd, crn)
+            names.Add(columns[0]);
+            users.Add(new User(columns[0], columns[4], columns[2]));
+        }
+
+        // Load in CRN - Lab/JsonUrl dictionary
+        lines = System.IO.File.ReadAllLines(crnPath);            
+
+        // 0: CRN, Odd: Lab Name, Even: JsonUrl associated with lab
+        for (int i = 1; i < lines.Length; i++)  // Skips labeling row
+        {
+            // Split row into columns 
+            string[] columns = lines[i].Split(',');
+            
+            // Create new Dictionary and add values for each pair in the row 
+            var tempDic = new Dictionary<string, string>();
+            try
+            {
+                for (int j = 1; j < columns.Length; j++)
+                {
+                    tempDic.Add(columns[j], columns[++j]);
+                }
+            } 
+            catch 
+            { 
+                Debug.Log("crn " + columns[0] + " has an incomplete crn/jsonpath pair.");
+            }
+
+            // Prints CRN with associated dictionary values. If there are multiple lines dedicated to the same crn, they will print on different lines 
+            // written on one line for quick toggling 
+            // string strs = ""; foreach (string s in tempDic.Keys) { strs += "Lab: " + s + "\t url: " + tempDic[s] + "\n"; } print(strs);
+
+            // Adds crn and Lab/json to dictionary unless it exists => adds to existing
+            try { 
+                result.Add(columns[0], tempDic);
+            }
+            catch {
+                foreach (string str in tempDic.Keys) 
+                { 
+                    result[columns[0]].Add(str, tempDic[str]); 
+                } 
+            }
+        }
+
+        return result;
+    }
+
+
+    // Sorts names in the autofilled list
+    private void sort(List<string> names)
+    {
+        names.Sort();                                               // used for List<string> implementation 
+        // Array.Sort(names, (s1, s2) => String.Compare(s1, s2));   // used for string[] implementation
+    }
+    #endregion
+}
+
+
+/* Used to store Student Usernames, Passwords, and CRNs as of now 
+ * All objects are added to a list where they can be searched
+*/
+class User 
+{
+    public string usr, pas, crn;
+
+    public User(string usr, string pas, string crn)
+    {
+        this.usr = usr;
+        this.pas = pas;
+        this.crn = crn; 
+    }
+
+    public override string ToString() { return ("Username: " + usr + "\tPassword: " + pas + "\t\tCRN: " + crn); }
+}
---
+++ _Code Device/AR Labs/Assets/Login Scene/loginLogic.cs
@@ -0,0 +1,388 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class loginLogic : MonoBehaviour
+{
+    #region Public Variables
+    public GameObject LoginUI; 
+    public GameObject usr;
+    public GameObject pas;
+    public GameObject loading;
+    public GameObject intro;
+    public GameObject keyboard;
+    public GameObject anchor; 
+    public GameObject placement_prop;
+    public GameObject controller;
+    public GameObject lab_options;
+    public GameObject guestButton;
+    public GameObject labTemp;
+    #endregion
+
+    #region Private Variables 
+    private List<GameObject> labList;
+    Dictionary<string, string> labOptions;
+    private string labSelected = "none";
+    private bool placed = false;
+    private bool playAnimation = true;
+    private bool setAnimationAnchor = true;
+    private int currState = -1; 
+    private enum state
+    { 
+        placement,
+        usr_entry,
+        pass_entry,
+        authentication,
+        lab_selection, 
+        lab_initiation,
+        end_of_states
+    }
+    #endregion
+
+    #region MonoBehaviour
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        labList = new List<GameObject>();
+        StartCoroutine(DownloadFile("http://cyberlearnar.cs.mtsu.edu/show_uploaded/test_names.csv","Assets/Login Scene/csv bank/test_names.csv"));
+        StartCoroutine(DownloadFile("http://cyberlearnar.cs.mtsu.edu/show_uploaded/crn_to_labs.csv","Assets/Login Scene/csv bank/crn_to_labs.csv"));
+        intro.SetActive(true);
+        toggleLineRender(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // After Start, sets intro position - such that it exists in worldspace and isn't bound to head movemnet
+        if (setAnimationAnchor && Camera.main.transform.position != new Vector3(0, 0, 0)) // This took longer than it should've to come up with
+        {
+            intro.transform.position = Camera.main.transform.position + Camera.main.transform.rotation * new Vector3(0, 0, 5); 
+            intro.transform.eulerAngles = new Vector3(0,Camera.main.transform.eulerAngles.y,0);
+            setAnimationAnchor = false;
+            toggleLineRender(false);
+        }
+
+        // After initial animation, this will initiate placement scene, then the login screen 
+        // intro.active throws warning, but don't trust the stinky computer
+        if (playAnimation && intro.active && intro.gameObject.transform.GetChild(0).GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            // prevents update from checking if intro is playing every frame
+            playAnimation = false;
+
+            // starts the rest of events in motion
+            print("Animation at idle; starting placement scene. :)\n");
+            next();
+        }
+
+        // place the scene object to set the coordinant system
+        if (placement_prop.active && !placed)
+        {
+            // Same as realign() - CONSIDERING REWORK
+            anchor.transform.position = controller.transform.position;
+            anchor.transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
+        } 
+    }
+    #endregion
+
+    #region Public Events
+    // OnHomeButtonDown() realigns UI to position of controller and angle head is pointing
+    public void realign()  // TODO I WANT TO FIX THIS RUNNING OUT OF TIME THO :(
+    {
+        print("Realigning UI.");
+        anchor.transform.position = controller.transform.position;
+        anchor.transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
+
+        // Alternative to align closer to the body - UNTESTED
+        // anchor.transform.position = new Vector3((controller.transform.position.x + Camera.main.transform.position.x)/2, controller.transform.position.y,(controller.transform.position.x + Camera.main.transform.position.x)/2);
+
+        // change orientation of starfield
+        // **Starfield disabled for now due to incapatibility**
+        // GameObject.Find("Starfield").transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
+    }
+
+
+    // Keep the flow of events involving the Login UI
+    public void next()
+    {
+        print("Current state: " + (state)(++currState) + "\n========================");
+
+        switch (currState)  
+        {
+            case (int)state.placement: // Placement scene: 0
+                {
+                    // disable linerenderer and MTSU model to look cleaner
+                    toggleLineRender(false);
+                    intro.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+
+                    // Destroy Lab Selection Options if looped through to beginning - MOVEABLE
+                    foreach (GameObject o in labList) { Destroy(o); }
+
+                    // Start placement scene and ensure it hasn't already been anchored before it started 
+                    placement_prop.SetActive(true);
+                    placed = false;
+                    break;
+                }
+
+            case (int)state.usr_entry: // User Entry: 1
+                {
+                    // Cleanup placement object
+                    toggleLineRender(true);
+                    placement_prop.SetActive(false);
+
+                    // Cleanup failed authentication
+                    loading.SetActive(false);
+                    print("clearing usernames\n");
+                    usr.GetComponent<Dropdown>().options.Clear();
+
+                    // Start UI
+                    LoginUI.SetActive(true);
+                    usr.SetActive(true);
+                    keyboard.SetActive(true);
+                    guestButton.SetActive(true);
+                    pas.SetActive(false);
+                    break;
+                }
+
+            case (int)state.pass_entry: // Pass Entry: 2
+                {
+                    // Disable User entry
+                    usr.SetActive(false);
+
+                    // Enable Pass Entry
+                    pas.SetActive(true);
+
+                    // Sets the keyboard text box to the Password Text box
+                    keyboard.GetComponent<VRKeyboard.Utils.KeyboardManager>().setText(pas.gameObject.transform.GetChild(0).GetComponent<Text>());
+                    break;
+                }
+
+            case (int)state.authentication: // Authentication: 3
+                {
+                    // Cleanup in-case authentication fails
+                    usr.GetComponent<autofill>().refreshText();
+                    keyboard.GetComponent<VRKeyboard.Utils.KeyboardManager>().resetText();
+
+                    // Submit request
+                    pas.SetActive(false);
+                    keyboard.SetActive(false);
+                    guestButton.SetActive(false);
+                    loading.SetActive(true);
+
+
+                    // THE AUTHENTICATION GOES HERE
+                    authenticate(usr.transform.GetChild(0).GetComponent<Text>().text, pas.transform.GetChild(0).GetComponent<Text>().text);
+                    break;
+                }
+
+            case (int)state.lab_selection: // Lab selection: 4
+                {
+                    // Disable UI and Keyboard 
+                    LoginUI.SetActive(false);
+                    keyboard.SetActive(false);
+
+                    // Load Modules 
+                    lab_options.SetActive(true);
+                    setLabs();
+                    break;
+                }
+
+            case (int)state.lab_initiation: // Insantiate lab: 5
+                {
+                    // Disable all UI
+                    lab_options.SetActive(false);
+
+                    // Start Lab Manager
+                    startLab();
+                    break;
+                }
+
+                // Catch if looped and extends past defined states 
+                // returns back to placement scene
+            default:
+                {
+                    // Disable lab selection
+                    lab_options.SetActive(false);
+                    print(labSelected);
+
+                    // Loop back to Start
+                    gotoState(0);
+                    break;
+                }
+        }
+    }
+
+
+    // Anchors scene to location of controller 
+    public void place()
+    {
+        // Ensures that prop isn't anchored before the placement scene
+        if (placement_prop.active && !placed)
+        {
+            print("Scene has been placed\n");
+            placed = true;
+            next(); 
+        }
+    }
+
+
+    // Called to automatically log in as "guest"
+    public void guestLogin()
+    {
+        print("Guest button hit; logging in as Guest user.");
+        // Fill in usr/pas perameters so no errors will be thrown 
+        // Also disables placeholders so it doesn't look ugly if user didn't type in user or pas perameters and has to retern to start
+        usr.transform.GetChild(0).GetComponent<Text>().text = "guest";
+        usr.transform.GetChild(1).gameObject.SetActive(false);
+        pas.transform.GetChild(0).GetComponent<Text>().text = "guest";
+        pas.transform.GetChild(1).gameObject.SetActive(false);
+
+        // Goes through normal authentication after filling in both perameters
+        gotoState((int)state.authentication);
+    }
+    #endregion
+
+    #region Private Events
+    // Downloads CSVs before autofill script on usr_dropbox is instantiated 
+    IEnumerator DownloadFile(string webpath, string path)
+    {
+        var uwr = new UnityWebRequest(webpath, UnityWebRequest.kHttpVerbGET);
+        uwr.downloadHandler = new DownloadHandlerFile(path);
+        yield return uwr.SendWebRequest();
+        if (uwr.result != UnityWebRequest.Result.Success)
+            Debug.LogError(uwr.error);
+        else
+            Debug.Log("File successfully downloaded and saved to " + path + "\n");
+    }
+
+
+    // Toggles line renderer emitted from controller
+    private void toggleLineRender(bool flag)
+    {
+        // print("doing the dirty work [|8^(");
+        controller.GetComponent<LineRenderer>().enabled = flag;
+        controller.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = flag;
+    }
+
+
+    // Called to change state if the state requested cannot be reached calling next();
+    private void gotoState(int state)
+    {
+        currState = state-1;  
+        next();
+    }
+
+
+    // Calls script in autofill to authenticate based on usr/pas logged 
+    private void authenticate(string usr, string pas) {
+        if (this.usr.GetComponent<autofill>().authenticate(usr, pas))
+            gotoState((int)state.lab_selection);
+        else
+            gotoState((int)state.usr_entry);
+
+        // One-line alternative
+        // gotoState((int)(this.usr.GetComponent<autofill>().authenticate(usr, pas)) ? state.modules : state.usr_entry);
+    }
+
+
+    // Instantiates UP TO 6 lab options that are clickable and load 
+    private void setLabs()
+    {
+        // pull labs as a Dictionary <string lab_name, string jsonUrl> 
+        //              username-field    script        method               (username text)
+        labOptions = usr.GetComponent<autofill>().getLabs(usr.transform.GetChild(0).GetComponent<Text>().text);
+
+        // Loop through labs pulling up to 5 of them and instantiating an interface to pick one
+        int count = 0;
+        foreach (string lab in labOptions.Keys)
+        {
+            labList.Add(createLab(lab, getDesc(labOptions[lab]), count++));
+            if (count == 5) { break; }
+        }
+
+        // Create one last button that allows user to exit application
+        labList.Add(createLab("Exit", "Closes program and sends data back to server", count++));
+    }
+
+    // Used by SetLabs() to instantiate variants of lab buttons to select lab
+    private GameObject createLab(string lab, string description, int count)
+    {
+        // Create instance and position it
+        GameObject newlab = Instantiate(labTemp, lab_options.transform);
+        newlab.transform.position += .42f * (count % 2) * anchor.transform.right + new Vector3(0, -.15f * (count / 2), 0);
+
+        // Give each lab a unique value onClick
+        newlab.GetComponent<Button>().onClick.AddListener(delegate () { setLab(lab); });
+
+        // Set Lab Title, description, name, and visibility
+        newlab.transform.GetChild(0).GetComponent<Text>().text = format(lab); //format doesn't work here but it really doesn't matter
+        newlab.transform.GetChild(1).GetComponent<Text>().text = description;
+        newlab.name = lab.Equals("Exit") ? lab : "Lab: " + lab;
+        newlab.SetActive(true);
+        return newlab;
+    }
+
+    // Removes underscores and replaces them with spaces
+    private string format(string lab)
+    {
+        char[] chars = lab.ToCharArray();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (chars[i] == '_') { chars[i] = ' '; }
+        }
+        return new string(chars);
+    }
+
+
+    // Returns lab description as a string  TODO
+    private string getDesc(string jsonUrl)
+    {
+        // string temp = jsonUrl.Substring(jsonUrl.indexOf("Description: {"));
+        // return jsonUrl.Substring(temp, temp.indexOf("}"));
+        return jsonUrl;
+    }
+
+
+    // Called when a lab is clicked on; sets value of (string) labSelected
+    private void setLab(string lab) 
+    {
+        labSelected = lab;
+        print("Lab Selected: " + format(labSelected) + "\n");
+        next();
+    }
+
+
+    // Instantiates the lab selected
+    private void startLab()
+    {
+        // If exit is selected from the list, End program here
+        if (labSelected.ToUpper().Equals("EXIT")) 
+        {
+            print("Exit tab selected; Exiting application");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+        } 
+
+        // If no lab has been selected (forced next()), returns to lab selection
+        else if (labSelected.Equals("none")) 
+        {
+            print("No lab selected; returning to lab selection");
+            gotoState((int)state.lab_selection); 
+        }
+
+        else
+        {
+            print("Lab selected: " + format(labSelected) + ", but no info to load for now :^)\n");
+            var manifestPath = "http://cyberlearnar.cs.mtsu.edu/lab_manifest/" + labSelected;
+            var jsonPath = labOptions[labSelected];
+            // GameObject.Find("LabManager").GetComponent<LabManagerScript>().startLab(manifestPath,jsonPath);
+        }
+    }
+#endregion
+}
