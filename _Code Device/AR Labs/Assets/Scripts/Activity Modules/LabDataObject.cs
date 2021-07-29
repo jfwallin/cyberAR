bb279413fd4c8dbb24c3cbee7125753f28d52f4b
Nicok01 <85763886+Nico-Kasz@users.noreply.github.com>
2021-07-29 11:08:34 -0500

    Update LabDataObject.cs

--- _Code Device/AR Labs/Assets/Scripts/Activity Modules/LabDataObject.cs
+++ _Code Device/AR Labs/Assets/Scripts/Activity Modules/LabDataObject.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 [System.Serializable]
 public class LabDataObject
 {
+    public string Lab_ID;
     public string Author;
     public string CourseName;
     public string EstimatedLength;
     public string NumModules;
     public string[] Objectives;
     public string[] ActivityModules;
+    public subManifest[] Assets;
+
+    public class subManifest
+    {
+        public string resource_id;
+        public string resource_type;
+        public string resource_url;
+    }
 }
